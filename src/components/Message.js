588f6db5f0b41d7f89d00c63d5f6309ec4ef3f44
Tawnee <allprt89@gmail.com>
2021-01-31 15:43:38 -0700

    add edit and connect edit to firebase.

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,15 +1,63 @@
+import { useState, useEffect, useCallback } from 'react'
 import { useDispatch } from 'react-redux'
-import { startDeleteMessage } from '../actions/message'
+import { startDeleteMessage, startEditMessage } from '../actions/message'
 
 const Message = ({ content, user, id }) => {
 	const dispatch = useDispatch()
+	const [canEdit, setCanEdit] = useState(false)
+	const [message, setMessage] = useState(content)
+	const [editedMessage, setEditedMessage] = useState(message)
+
+	const handleEscape = useCallback(
+		(e) => {
+			if (e.key === 'Escape') {
+				setCanEdit(false)
+				setEditedMessage(message)
+			}
+		},
+		[message]
+	)
+
+	const handleKeypress = (e) => {
+		if (e.key === 'Enter' && canEdit) {
+			setCanEdit(false)
+			setMessage(editedMessage)
+
+			dispatch(
+				startEditMessage(id, {
+					user,
+					content: editedMessage,
+				})
+			)
+		}
+	}
+
+	useEffect(() => {
+		if (!canEdit) return
+		window.addEventListener('keydown', handleEscape)
+		return () => window.removeEventListener('keydown', handleEscape)
+	}, [canEdit, handleEscape])
 
 	return (
 		<div className='message'>
 			<div className='message-text'>
 				<h4>{user}</h4>
-				<p>{content}</p>
+				{canEdit ? (
+					<input
+						type='text'
+						className='edit-input'
+						value={editedMessage}
+						onChange={(e) => setEditedMessage(e.target.value)}
+						onKeyPress={handleKeypress}
+					/>
+				) : (
+					<p>{message}</p>
+				)}
 			</div>
+
+			<button className='message-button' onClick={() => setCanEdit(true)}>
+				edit
+			</button>
 
 			<button
 				className='message-button'
